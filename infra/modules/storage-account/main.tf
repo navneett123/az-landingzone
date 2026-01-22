f5b97575208d34246b926579b751507ac3fe99a7
navneett123 <learn.devops.navneet@gmail.com>
2026-01-22 23:25:31 +0530

    modifying the storage module

--- infra/modules/storage-account/main.tf
+++ infra/modules/storage-account/main.tf
@@ -1,6 +1,8 @@
-variable "name" {}
+
 variable "resource_group_name" {}
 variable "location" {}
+variable "storage_account_name"{type=string}
+
 
 resource "azurerm_storage_account" "sa" {
   name                     = var.name
