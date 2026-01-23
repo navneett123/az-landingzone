0bcfd42d040ac0de6a1abc9a988d8c01211465f0
navneett123 <learn.devops.navneet@gmail.com>
2026-01-23 15:23:29 +0530

    modified the iac core

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -29,7 +29,7 @@ module "subnet" {
   source               = "../../modules/subnet"
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.virtual_network_name
+  virtual_network_name = var.vnet_name
   address_prefixes     = var.address_prefixes
 }
 
