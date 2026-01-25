95c24b52d80ffe8b42883afb735586975b8d6e84
navneett123 <learn.devops.navneet@gmail.com>
2026-01-25 17:42:46 +0530

    added the module vm variables

---
+++ infra/modules/vm-linux/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+variable "public_ip_id" {
+  type = string
+}
+
+variable "nsg_id" {
+  type = string
+}
